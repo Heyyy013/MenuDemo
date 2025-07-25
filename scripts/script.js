f5b5ecc8cef1453c4c303f4ab899f55b2e45e6e9
Ali <zallitoaliinfo@gmail.com>
2025-07-25 12:31:49 +0200

    changement du header, bouton bas gauche + filtre arrondis

--- scripts/script.js
+++ scripts/script.js
@@ -4,3 +4,19 @@ const nav = document.querySelector('nav');
 toggleBtn.addEventListener('click', () => {
     nav.classList.toggle('open');
 });
+
+let elements = document.querySelectorAll("[data-menu]");
+for (let i = 0; i < elements.length; i++) {
+    let main = elements[i];
+
+    main.addEventListener("click", function () {
+        let element = main.parentElement.parentElement;
+        let indicators = main.querySelectorAll("img");
+        let child = element.querySelector("#menu");
+        let h = element.querySelector("#mainHeading>div>p");
+
+        h.classList.toggle("font-semibold");
+        child.classList.toggle("hidden");
+        indicators[0].classList.toggle("rotate-180");
+    });
+}
